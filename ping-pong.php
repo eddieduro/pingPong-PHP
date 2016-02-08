3f81980e54a7ea430c66d919f80480b7ace86fc1
eddie <e.jamesduro@gmail.com>
2016-02-07 22:11:44 -0800

    upload files

---
+++ ping-pong.php
@@ -0,0 +1,39 @@
+<?php
+	$start_number = $_GET['start_number'];
+	$end_number = $_GET['ending_number'];		
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ping Pong</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<!-- Stylesheets -->
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+	<div class="container">
+		<ul>
+			<?php
+				foreach(range($start_number, $end_number) as $number){
+					if($number % 3 == 0 && $number % 5 == 0) {
+						echo "<li> ping-pong </li>";  
+					} elseif($number % 3 == 0) {
+						echo "<li> ping </li>";  
+					} elseif($number % 5 == 0) {
+						echo "<li> pong </li>";  
+					} else {
+						echo "<li>" . $number . "</li>";
+					}
+				}
+			?>
+		</ul>
+	</div>
+	<!-- Scripts -->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+</body>
+</html>
